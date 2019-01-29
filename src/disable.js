989f1a9b8a2f4902f056b28dd4b474df9c0da774
borisc1996 <borisc1996@gmail.com>
2019-01-29 14:31:00 -0500

    :recycle: refactored to remove jquery dependency

--- src/disable.js
+++ src/disable.js
@@ -1,19 +1,54 @@
+import Stats from './stats';
+
+const q = document.getElementById('q');
+const w = document.getElementById('w');
+const e = document.getElementById('e');
+const r = document.getElementById('r');
+const ba = document.getElementById('basic-attack');
+const eq = document.getElementById('equipment-new');
+const s = document.getElementById('equipment-shop');
+
 export const disable = function () {
-  $('.basic-attack').prop('disabled', true);
-  $('.q').prop('disabled', true);
-  $('.w').prop('disabled', true);
-  $('.e').prop('disabled', true);
-  $('.r').prop('disabled', true);
-  $('#equipment-new').prop('disabled', true);
-  $('#equipment-shop').prop('disabled', true);
+  ba.disabled = true;
+  q.disabled = true;
+  w.disabled = true;
+  e.disabled = true;
+  r.disabled = true;
+  eq.disabled = true;
+  s.disabled = true;
 };
 
 export const enable = function () {
-  $('.basic-attack').prop('disabled', false);
-  $('.q').prop('disabled', false);
-  $('.w').prop('disabled', false);
-  $('.e').prop('disabled', false);
-  $('.r').prop('disabled', false);
-  $('#equipment-new').prop('disabled', false);
-  $('#equipment-shop').prop('disabled', false);
+  ba.disabled = false;
+
+  if (Stats.playerSpells[0].cost > Stats.playerMana) {
+    q.classList.add('no-mana');
+  } else {
+    q.classList.remove('no-mana');
+    q.disabled = false;
+  }
+
+  if (Stats.playerSpells[1].cost > Stats.playerMana) {
+    w.classList.add('no-mana');
+  } else {
+    w.classList.remove('no-mana');
+    w.disabled = false;
+  }
+
+  if (Stats.playerSpells[2].cost > Stats.playerMana) {
+    e.classList.add('no-mana');
+  } else {
+    e.classList.remove('no-mana');
+    e.disabled = false;
+  }
+
+  if (Stats.playerSpells[3].cost > Stats.playerMana) {
+    r.classList.add('no-mana');
+  } else {
+    r.classList.remove('no-mana');
+    r.disabled = false;
+  }
+  
+  eq.disabled = false;
+  s.disabled = false;
 };
