d965200c3f0c6c90441c01b712b2d4da9e425551
borisc1996 <borisc1996@gmail.com>
2019-05-06 23:14:44 +0100

    :art: add armoury interface logic.

--- src/menu/armoury-interface.js
+++ src/menu/armoury-interface.js
@@ -1,14 +1,73 @@
+/* eslint-disable no-restricted-syntax */
+import Items from './wizard-item-store';
+
 let selectedItemType = null;
+
+const rings = ['ferocious-ring', 'guardian-ring', 'arcane-ring', 'seers-ring',];
 
 const typeArmour = document.getElementById('type-armour');
 const typeTrinket = document.getElementById('type-trinket');
 const typeRing = document.getElementById('type-ring');
 
-const updateItemDescription = () => {
+const itemName = document.getElementById('item-name');
+const itemIcon = document.getElementById('item-icon');
+const itemDesc = document.getElementById('item-desc-main');
+const itemLore = document.getElementById('item-desc-lore');
 
+const grid = document.getElementById('item-grid');
+
+const updateItemDescription = (e) => {
+  const selected = document.querySelector('.item-selected');
+  if (selected !== null) {
+    selected.classList.remove('item-selected');
+  }
+  e.target.parentElement.classList.add('item-selected');
+
+  const item = Items.getItem(e.target.id);
+  itemIcon.src = item.src;
+  itemName.innerText = item.name;
+  itemDesc.innerHTML = item.desc;
+  itemLore.innerText = item.lore;
+};
+
+const removeItems = () => {
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
+};
+
+const setItemsFromArray = (items) => {
+  removeItems();
+  let count = 1;
+  // eslint-disable-next-line guard-for-in
+  for (let i in items) {
+    // max items = 12.
+    if (count > 12) {
+      throw new Error('Too many items.');
+    }
+    const container = document.createElement('div');
+    container.classList.add('item', `item-${count}`);
+    container.id = `item-${count}`;
+    const image = document.createElement('img');
+    image.src = items[i].src;
+    image.id = items[i].id;
+    image.classList.add('item-size');
+    image.addEventListener('click', updateItemDescription);
+
+    container.appendChild(image);
+    grid.appendChild(container);
+    // select 1st item.
+    if (count === 1) {
+      image.click();
+    }
+    count += 1;
+  }
 };
 
 const updateItemType = (e) => {
+  if (selectedItemType === e.target.id) {
+    return;
+  }
   selectedItemType = e.target.id;
 
   typeArmour.parentElement.classList.remove('type-selected');
@@ -24,10 +83,13 @@ const updateItemType = (e) => {
 
   if (selectedItemType === 'type-armour') {
     typeArmour.src = '/res/interface/armoury/armour-active.png';
+    setItemsFromArray(Items.armour);
   } else if (selectedItemType === 'type-trinket') {
     typeTrinket.src = '/res/interface/armoury/trinket-active.png';
+    setItemsFromArray(Items.trinkets);
   } else if (selectedItemType === 'type-ring') {
     typeRing.src = '/res/interface/armoury/ring-active.png';
+    setItemsFromArray(Items.rings);
   } else {
     throw new Error(e.target.id);
   }
--- src/menu/wizard-item-store.js
+++ src/menu/wizard-item-store.js
@@ -1,62 +1,67 @@
-const rings = [
-  {
+import {
+  icon,
+  text
+} from '../icons';
+
+const rings = {
+  ferocious: {
     name: 'Ferocious Ring',
     id: 'ferocious-ring',
     src: 'res/item/amulet/ferocious-ring.png',
-    desc: `+ 3 ${damage}`,
+    desc: `+ ${icon.damage}${text.damage(3)}`,
     lore: 'The ring\'s bright ruby color is unmistakable on the battlefield.',
     cost: 50,
     locked: true,
   },
-  {
+  guardian: {
     name: 'Guardian Ring',
     id: 'guardian-ring',
     src: 'res/item/amulet/guardian-ring.png',
-    desc: `+ 2 ${ac}`,
+    desc: `+ ${icon.armour}${text.armour(2)}`,
     lore: 'A glimmering ring that defends its bearer.',
     cost: 50,
     locked: true,
   },
-  {
+  arcane: {
     name: 'Arcane Ring',
     id: 'arcane-ring',
     src: 'res/item/amulet/arcane-ring.png',
-    desc: `+ 2 ${runic}`,
+    desc: `+ ${icon.runic}${text.runic(2)}`,
     lore: 'Its gem was once used to channel void realm magic, this ring pulses with energy.',
     cost: 50,
     locked: true,
   },
-  {
+  seers: {
     name: 'Seers Ring',
     id: 'seers-ring',
     src: 'res/item/amulet/seers-ring.png',
-    desc: `+ 50 ${mana}`,
+    desc: `+ 50 Mana`,
     lore: 'A ring that grants more mana to its bearer.',
     cost: 50,
     locked: true,
   },
-  {
+  artemis: {
     name: 'Artemisian Ring',
     id: 'artemis-ring',
     src: 'res/item/amulet/artemis-ring.png',
-    desc: `+ 2 ${hit}`,
+    desc: `+ 2 hit`,
     lore: 'This ring is considered a good luck charm among the elves.',
     cost: 50,
     locked: true,
   },
-];
+};
 
-const armour = [
-  {
+const armour = {
+  circe: {
     name: 'Robe of Circe',
     id: 'circe-armour',
     src: 'res/item/armour/circe-armour.png',
-    desc: `Increase to 50 ${mana} regeneration per turn.`,
+    desc: `Increase to 50 mana regeneration per turn.`,
     lore: 'This elegant garment was once worn by a powerful enchantress.',
     cost: 50,
     locked: true,
   },
-  {
+  colossus: {
     name: 'Mantle of the Colossus',
     id: 'colossus-armour',
     src: 'res/item/armour/colossus-armour.png',
@@ -65,16 +70,16 @@ const armour = [
     cost: 50,
     locked: true,
   },
-  {
+  valkyrie: {
     name: 'Valkyrie Armour',
     id: 'valkyrie-armour',
     src: 'res/item/armour/valkyrie-armour.png',
-    desc: `Heal for 1d4 per turn.`,
+    desc: `Heal for 4 per turn.`,
     lore: 'Legendary armour worn by the Valkyries known for its regenerative magic.',
     cost: 50,
     locked: true,
   },
-  {
+  daedric: {
     name: 'Daedric Carapace',
     id: 'daedric-armour',
     src: 'res/item/armour/daedric-armour.png',
@@ -83,7 +88,7 @@ const armour = [
     cost: 50,
     locked: true,
   },
-  {
+  leviathan: {
     name: 'Leviathan Chestplate',
     id: 'leviathan-armour',
     src: 'res/item/armour/leviathan-armour.png',
@@ -92,64 +97,64 @@ const armour = [
     cost: 50,
     locked: true,
   },
-];
+};
 
-const trinkets = [
-  {
+const trinkets = {
+  archeron: {
     name: 'Archeron War Boots',
     id: 'archeron-trinket',
     src: 'res/item/trinket/archeron-trinket.png',
-    desc: `Increase ${damage} by 4, but decrease ${ac} by 2.`,
+    desc: `Increase ${icon.damage}${text.damage(4)}, but decrease ${icon.armour}${text.armour(2)}.`,
     lore: 'The boots will make the wearer have no fear of combat.',
     cost: 50,
     locked: true,
   },
-  {
+  achilles: {
     name: 'Achilles Helm',
     id: 'achilles-trinket',
     src: 'res/item/trinket/achilles-trinket.png',
-    desc: `Increase ${ac} by 6, but decrease ${hit} by 3.`,
+    desc: `Increase ${icon.armour}${text.armour(6)}, but decrease hit by 3.`,
     lore: 'The helmet of a legendary warrior who fell in battle.',
     cost: 50,
     locked: true,
   },
-  {
+  asgardian: {
     name: 'Asgardian Steel Gauntlet',
     id: 'asgardian-trinket',
     src: 'res/item/trinket/asgardian-trinket.png',
-    desc: `Increase ${hit} by 2, but decrease ${ac} by 2.`,
+    desc: `Increase hit by 2, but decrease ${icon.armour}${text.armour(2)}.`,
     lore: 'Celestial magic is still remnant from the valkyrie who once wore it.',
     cost: 50,
     locked: true,
   },
-  {
+  fates: {
     name: 'Amulet of the Fates',
     id: 'fates-trinket',
     src: 'res/item/trinket/fates-trinket.png',
-    desc: `Your spells will have 100% ${hit}, but have twice the ${mana} cost.`,
+    desc: `Your spells will have 100% hit, but have twice the mana cost.`,
     lore: 'A divine amulet used to influence the nature of spells.',
     cost: 50,
     locked: true,
   },
-  {
+  black: {
     name: 'Black Star',
     id: 'black-trinket',
     src: 'res/item/trinket/black-trinket.png',
-    desc: `Gain 1 ${runic} per every 20% of missing health.`,
+    desc: `Gain ${icon.runic}${text.runic(1)} per every 20% of missing health.`,
     lore: 'An enigmatic amulet made from a cracked azure crystal, often used in rituals pertaining to dark magics.',
     cost: 50,
     locked: true,
   },
-  {
+  elven: {
     name: 'Glyph of Elvenkind',
     id: 'elven-trinket',
     src: 'res/item/trinket/elven-trinket.png',
-    desc: `Grants a 25% chance to lower enemy ${ac} by 1, down by a maximum of 10.`,
+    desc: `Grants a 25% chance to lower enemy ${icon.armour}${text.armour(1)}, down by a maximum of 10.`,
     lore: 'An enchanted glyph worn by the elves to bring good fortune in battle.',
     cost: 50,
     locked: true,
   },
-  {
+  book: {
     name: 'Book of War',
     id: 'book-trinket',
     src: 'res/item/trinket/book-trinket.png',
@@ -158,7 +163,7 @@ const trinkets = [
     cost: 50,
     locked: true,
   },
-  {
+  oblivion: {
     name: 'Oblivion Stone',
     id: 'oblivion-trinket',
     src: 'res/item/trinket/oblivion-trinket.png',
@@ -167,7 +172,7 @@ const trinkets = [
     cost: 50,
     locked: true,
   },
-  {
+  void: {
     name: 'Rune of the Void',
     id: 'void-trinket',
     src: 'res/item/trinket/void-trinket.png',
@@ -176,7 +181,7 @@ const trinkets = [
     cost: 50,
     locked: true,
   },
-  {
+  silverhawk: {
     name: 'Silverhawk Feather',
     id: 'silverhawk-trinket',
     src: 'res/item/trinket/silverhawk-trinket.png',
@@ -185,22 +190,80 @@ const trinkets = [
     cost: 50,
     locked: true,
   },
-  {
+  mystra: {
     name: 'Skull of Mystra',
     id: 'mystra-trinket',
     src: 'res/item/trinket/mystra-trinket.png',
-    desc: `Increase ${runic} by 4, but decrease ${ac} by 4.`,
+    desc: `Increase ${icon.runic}${text.runic(4)} by 4, but decrease ${icon.armour}${text.armour(4)}.`,
     lore: 'An artefact of unknown origin which emanates dark magical power.',
     cost: 50,
     locked: true,
   },
-  {
+  warbelt: {
     name: 'Warbelt of Radiance',
     id: 'warbelt-trinket',
     src: 'res/item/trinket/warbelt-trinket.png',
-    desc: `Passively deal bonus 2 ${damage} on succesful hits.`,
+    desc: `Passively deal bonus ${icon.damage}${text.damage(2)} on succesful hits.`,
     lore: 'A warbelt that causes damage and a bright burning effect that lays waste to nearby enemies.',
     cost: 50,
     locked: true,
   },
-];
+};
+
+const items = {
+  rings,
+  armour,
+  trinkets,
+  getItem(name) {
+    switch (name) {
+      case 'ferocious-ring':
+        return rings.ferocious;
+      case 'guardian-ring':
+        return rings.guardian;
+      case 'arcane-ring':
+        return rings.arcane;
+      case 'seers-ring':
+        return rings.seers;
+      case 'artemis-ring':
+        return rings.artemis;
+      case 'circe-armour':
+        return armour.circe;
+      case 'colossus-armour':
+        return armour.colossus;
+      case 'valkyrie-armour':
+        return armour.valkyrie;
+      case 'daedric-armour':
+        return armour.daedric;
+      case 'leviathan-armour':
+        return armour.leviathan;
+      case 'archeron-trinket':
+        return trinkets.archeron;
+      case 'achilles-trinket':
+        return trinkets.achilles;
+      case 'asgardian-trinket':
+        return trinkets.asgardian;
+      case 'fates-trinket':
+        return trinkets.fates;
+      case 'black-trinket':
+        return trinkets.black;
+      case 'elven-trinket':
+        return trinkets.elven;
+      case 'book-trinket':
+        return trinkets.book;
+      case 'oblivion-trinket':
+        return trinkets.oblivion;
+      case 'void-trinket':
+        return trinkets.void;
+      case 'silverhawk-trinket':
+        return trinkets.silverhawk;
+      case 'mystra-trinket':
+        return trinkets.mystra;
+      case 'warbelt-trinket':
+        return trinkets.warbelt;
+      default:
+        throw new TypeError('Item not found');
+    }
+  },
+};
+
+export default items;
